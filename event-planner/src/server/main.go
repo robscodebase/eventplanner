c89e6a51b039ff74cb3514630ad0f8ff05f9680a
Robert Reyna <robert@moasis.com>
2018-02-05 17:15:11 -0500

    Successful html template integration. TODO: Write tests.

--- event-planner/src/server/main.go
+++ event-planner/src/server/main.go
@@ -7,11 +7,17 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/gorilla/handlers"
 	"net/http"
+	"html/template"
+    "io/ioutil"
 )
+var Login bool
 
 func main() {
 	runHandlers()
 }
+var (
+    homePage  = compileTemplate("home.html")
+)
 
 func runHandlers() {
 	r := mux.NewRouter()
@@ -24,8 +30,7 @@ func runHandlers() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) *errorMessage {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
-    return nil
+    return homePage.runTemplate(w, r, nil)
 }
 
 type errorCheck func(http.ResponseWriter, *http.Request) *errorMessage
@@ -50,4 +55,64 @@ func formatError(err error, format string, v ...interface{}) *errorMessage {
 		Message: fmt.Sprintf(format, v...),
 		Code:    500,
 	}
+}
+
+type eventPlannerTemplate struct {
+    eventPlannerTemplate *template.Template
+}
+
+func compileTemplate(templateName string) *eventPlannerTemplate {
+    // Add the main template file.
+    main := template.Must(template.ParseFiles("/go/src/event-planner/src/server/templates/main.html"))
+
+    // Add the header.
+    header := readFile("header.html")
+    // Add the body.
+    body := readFile(templateName)
+    // Add the footer.
+    footer := readFile("footer.html")
+
+    // Combine header, body, and footer and return template.
+    template.Must(main.New("header").Parse(string(header)))
+    template.Must(main.New("body").Parse(string(body)))
+    template.Must(main.New("footer").Parse(string(footer)))
+    return &eventPlannerTemplate{main.Lookup("main.html")}
+}
+
+func readFile(fileName string) []byte {
+    template, err := ioutil.ReadFile("/go/src/event-planner/src/server/templates/" + fileName)
+    if err != nil {
+        panic(fmt.Errorf("compileTemplate() could not read footer: %v", err))
+    }
+    return template
+}
+func (template *eventPlannerTemplate) runTemplate(w http.ResponseWriter, r *http.Request, input interface{}) *errorMessage {
+    session := struct {
+        Input        interface{}
+        AuthEnabled bool
+        UserData     *UserData
+    }{
+        Input:        input,
+        AuthEnabled: true,
+    }
+
+    if Login {
+         session.UserData = sessionData(r)
+    }
+
+    if err := template.eventPlannerTemplate.Execute(w, session); err != nil {
+        return formatError(err, "runTemplate() could not execute template: %v")
+    }
+    return nil
+}
+
+func sessionData(r *http.Request) *UserData {
+    return &UserData{
+        ID:          "SECURE ID",
+        UserName: "ADMIN",
+    }
+}
+
+type UserData struct {
+    ID, UserName string
 }

c82038e20fed22d3995052d7201d8dd25652582d
Robert Reyna <robert@moasis.com>
2018-02-05 16:37:16 -0500

    Golang server running on port 8080 in docker container. Added errorCheck to handlers. TODO: Integrate html template.

--- event-planner/run.sh
+++ event-planner/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo docker stop $(docker ps -a -q) && sudo docker rm $(docker ps -a -q)
 sudo docker build -t event-planner .
-sudo docker run -dp 8080:8080 --name event-planner
+sudo docker run -dp 8080:8080 --name event-planner event-planner
 sudo docker exec -ti event-planner /bin/bash
--- event-planner/src/server/main.go
+++ event-planner/src/server/main.go
@@ -1,57 +1,53 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
-
-    "github.com/gorilla/handlers"
-    "github.com/gorilla/mux"
+	"fmt"
+	"log"
+	"os"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+	"net/http"
 )
 
-type Login bool
-
 func main() {
-    registerHandlers()
+	runHandlers()
 }
 
-func registerHandlers() {
-    r := mux.NewRouter()
-    r.Handle("/", http.RedirectHandler("/event-planner", http.StatusFound))
-    r.Methods("GET").Path("/").
-        Handler(errorCheck(home))
-    http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, r))
+func runHandlers() {
+	r := mux.NewRouter()
+	r.Handle("/", http.RedirectHandler("/home", http.StatusFound))
+	r.Methods("GET").Path("/home").
+		Handler(errorCheck(home))
+	http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, r))
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func home(w http.ResponseWriter, r *http.Request) *errorMessage {
-    books, err := bookshelf.DB.ListBooks()
-    if err != nil {
-        return formatError(err, "could not list books: %v", err)
-    }
-    return compileTemplate.Execute(w, r, books)
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    return nil
 }
 
 type errorCheck func(http.ResponseWriter, *http.Request) *errorMessage
 
 type errorMessage struct {
-    Error   error
-    Message string
-    Code    int
+	Error   error
+	Message string
+	Code    int
 }
 
 func (errCheck errorCheck) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    if errcheck := errCheck(w, r); errcheck != nil {
-        log.Printf("serveHTTP error: status code: %d, message: %s, error: %#v",
-            errcheck.Code, errcheck.Message, errcheck.Error)
-        http.Error(w, errcheck.Message, errcheck.Code)
-    }
+	if errcheck := errCheck(w, r); errcheck != nil {
+		log.Printf("serveHTTP error: status code: %d, message: %s, error: %#v",
+			errcheck.Code, errcheck.Message, errcheck.Error)
+		http.Error(w, errcheck.Message, errcheck.Code)
+	}
 }
 
 func formatError(err error, format string, v ...interface{}) *errorMessage {
-    return &errorMessage{
-        Error:   err,
-        Message: fmt.Sprintf(format, v...),
-        Code:    500,
-    }
+	return &errorMessage{
+		Error:   err,
+		Message: fmt.Sprintf(format, v...),
+		Code:    500,
+	}
 }

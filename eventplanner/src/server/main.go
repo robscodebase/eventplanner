5051a7606b91659616a09eae241e78d8f7a9aeb2
Robert Reyna <robert@moasis.com>
2018-02-09 14:40:58 -0500

    Template functions for html template extracted from main.go into templates.go. TODO: Build out html and javascript on home page.

--- eventplanner/src/server/main.go
+++ eventplanner/src/server/main.go
@@ -4,12 +4,9 @@ import (
 	"fmt"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
 var Login bool
@@ -17,14 +14,6 @@ var filePathBase string
 
 func main() {
 	runHandlers()
-}
-
-func init() {
-	workingDirectory, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("can't open working directory. err %v:", err)
-	}
-	filePathBase = filepath.Base(workingDirectory)
 }
 
 var (
@@ -67,55 +56,6 @@ func formatError(err error, format string, v ...interface{}) *errorMessage {
 		Message: fmt.Sprintf(format, v...),
 		Code:    500,
 	}
-}
-
-type eventPlannerTemplate struct {
-	eventPlannerTemplate *template.Template
-}
-
-func compileTemplate(templateName string) *eventPlannerTemplate {
-	// Add the main template file.
-	main := template.Must(template.ParseFiles(filePathBase + "templates/main.html"))
-
-	// Add the header.
-	header := readFile("header.html")
-	// Add the body.
-	body := readFile(templateName)
-	// Add the footer.
-	footer := readFile("footer.html")
-
-	// Combine header, body, and footer and return template.
-	template.Must(main.New("header").Parse(string(header)))
-	template.Must(main.New("body").Parse(string(body)))
-	template.Must(main.New("footer").Parse(string(footer)))
-	return &eventPlannerTemplate{main.Lookup("main.html")}
-}
-
-func readFile(fileName string) []byte {
-	template, err := ioutil.ReadFile(filePathBase + "templates/" + fileName)
-	if err != nil {
-		panic(fmt.Errorf("compileTemplate() could not read footer: %v", err))
-	}
-	return template
-}
-func (template *eventPlannerTemplate) runTemplate(w http.ResponseWriter, r *http.Request, input interface{}) *errorMessage {
-	session := struct {
-		Input       interface{}
-		AuthEnabled bool
-		UserData    *UserData
-	}{
-		Input:       input,
-		AuthEnabled: true,
-	}
-
-	if Login {
-		session.UserData = sessionData(r)
-	}
-
-	if err := template.eventPlannerTemplate.Execute(w, session); err != nil {
-		return formatError(err, "runTemplate() could not execute template: %v")
-	}
-	return nil
 }
 
 func sessionData(r *http.Request) *UserData {
---
+++ eventplanner/src/server/templates.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func init() {
+	workingDirectory, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("can't open working directory. err %v:", err)
+	}
+	filePathBase = filepath.Base(workingDirectory)
+}
+
+type eventPlannerTemplate struct {
+	eventPlannerTemplate *template.Template
+}
+
+func compileTemplate(templateName string) *eventPlannerTemplate {
+	// Add the main template file.
+	main := template.Must(template.ParseFiles(filePathBase + "templates/main.html"))
+
+	// Add the header.
+	header := readFile("header.html")
+	// Add the body.
+	body := readFile(templateName)
+	// Add the footer.
+	footer := readFile("footer.html")
+
+	// Combine header, body, and footer and return template.
+	template.Must(main.New("header").Parse(string(header)))
+	template.Must(main.New("body").Parse(string(body)))
+	template.Must(main.New("footer").Parse(string(footer)))
+	return &eventPlannerTemplate{main.Lookup("main.html")}
+}
+
+func readFile(fileName string) []byte {
+	template, err := ioutil.ReadFile(filePathBase + "templates/" + fileName)
+	if err != nil {
+		panic(fmt.Errorf("compileTemplate() could not read footer: %v", err))
+	}
+	return template
+}
+
+func (template *eventPlannerTemplate) runTemplate(w http.ResponseWriter, r *http.Request, input interface{}) *errorMessage {
+	session := struct {
+		Input       interface{}
+		AuthEnabled bool
+		UserData    *UserData
+	}{
+		Input:       input,
+		AuthEnabled: true,
+	}
+
+	if Login {
+		session.UserData = sessionData(r)
+	}
+
+	if err := template.eventPlannerTemplate.Execute(w, session); err != nil {
+		return formatError(err, "runTemplate() could not execute template: %v")
+	}
+	return nil
+}
